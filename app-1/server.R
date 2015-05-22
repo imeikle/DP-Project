55bdbad4c87ee34856a3cbac42a4d0cd182a86e9
Ian Meikle <imeikle@gmail.com>
2015-05-22 18:11:08 +0100

    Final app

--- app-1/server.R
+++ app-1/server.R
@@ -2,16 +2,14 @@ mean <- 30
 
 
 shinyServer(
-    function(input, output) {
-        
-        output$powerValue <- renderPrint({input$power})
-        output$xbarValue <- renderPrint({input$xbar})
-        output$sdValue <- renderPrint({input$sigma})
-        
-        output$sample <- renderPrint({power.t.test(power = input$power,
-                        delta = input$xbar - mean,
-                        sd = as.numeric(input$sigma),
-                        type = "one.sample",
-                        alt = "one.sided")$n})
-    }
+        function(input, output) {
+                
+                output$powerValue <- renderPrint({input$power})
+                output$xbarValue <- renderPrint({input$xbar})
+                output$sdValue <- renderPrint({input$sigma})
+                
+                output$sample <- renderPrint({power.t.test(power = input$power,
+                                                           delta = input$xbar - mean,
+                                                           sd = as.numeric(input$sigma))$n})
+        }
 )
--- app-1/ui.R
+++ app-1/ui.R
@@ -1,33 +1,37 @@
 shinyUI(fluidPage(
-    titlePanel("Statistical Power"),
-
-  sidebarLayout(
-    sidebarPanel(
-        helpText("Set values for Power, Mean and Standard Deviation"),
+        titlePanel("Statistical Power"),
         
-        sliderInput("power",
-            label= "Power:",
-            min = 0.2, max = 1.0, value = 0.4, step = 0.1),
-        
-        numericInput("xbar",
-            label = "Sample mean:",
-            value = 32, min = 31, max = 40, step = 1),
-        
-        radioButtons("sigma",
-            label = "Standard deviation:",
-            choices = c(1,2,4,10),
-            selected = 2)
-        ),
-        
-        
-    mainPanel(
-        h3("Number of observations"),
-        
-        verbatimTextOutput("sample")
-        
-#         textOutput("text1"),
-#         textOutput("text2"),
-#         textOutput("text3")
-    )
-  )
+        sidebarLayout(
+                sidebarPanel(
+                        helpText("Population Mean is set at 30. Please set values 
+                                 for Power, Sample Mean and Standard Deviation"),
+                        
+                        sliderInput("power",
+                                    label= "Power:",
+                                    min = 0.2, max = 1.0, value = 0.4, step = 0.1),
+                        
+                        numericInput("xbar",
+                                     label = "Sample mean:",
+                                     value = 32, min = 31, max = 40, step = 1),
+                        
+                        radioButtons("sigma",
+                                     label = "Standard deviation:",
+                                     choices = c(1,2,4,10),
+                                     selected = 2)
+                ),
+                
+                
+                mainPanel(
+                        h3("Sample size"),
+                        p("The number of observations must be higher than:"),
+                        
+                        verbatimTextOutput("sample"),
+                        
+                        p("for the parameters specified.")
+                        
+                        #         textOutput("text1"),
+                        #         textOutput("text2"),
+                        #         textOutput("text3")
+                )
+        )
 ))
